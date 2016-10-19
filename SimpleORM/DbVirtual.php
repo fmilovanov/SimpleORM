aa15a076fd5d7a3bb31f33dd151f65e96f01d6b3
Felix A. Milovanov <fmilovanov@yahoo.com>
2016-10-19 11:06:25 -0400

    DbVirtual::update()

--- SimpleORM/DbVirtual.php
+++ SimpleORM/DbVirtual.php
@@ -5,10 +5,16 @@
 
 class DbVirtual implements IDbAdapter
 {
+    const ERROR_NO_TABLE        = 'no table found';
+    const ERROR_NO_KEY          = 'no key found';
+    const ERROR_TRANSACTION     = 'already in transaction';
+    const ERROR_NO_TRANSACTION  = 'no transaction started';
+
     private $_pdo;
 
     public $last_id;
     public $tables = array();
+    public $tables_transaction;
 
     public function __construct(PDO $pdo = null)
     {
@@ -28,9 +34,38 @@ class DbVirtual implements IDbAdapter
         $this->tables[$table][$this->last_id] = $data;
     }
 
+    private function _simpleMatch($table, array $where)
+    {
+        if (!isset($this->tables[$table]))
+            throw new \Exception(self::ERROR_NO_TABLE);
+
+        $ids = array();
+        foreach ($this->tables[$table] as $id => $data)
+        {
+            foreach ($where as $key => $value)
+            {
+                if (!array_key_exists($key, $data))
+                    throw new \Excpetion(self::ERROR_NO_KEY);
+
+                if ($value !== $data[$key])
+                    continue 2;
+            }
+
+            $ids[] = $id;
+        }
+
+        return $ids;
+    }
+
     public function update($table, array $data, array $where)
     {
-
+        foreach ($this->_simpleMatch($table, $where) as $id)
+        {
+            foreach ($data as $key => $value)
+            {
+                $this->tables[$table][$id][$key] = $value;
+            }
+        }
     }
 
     public function delete($table, array $where, $allow_delete_all = false)
@@ -45,17 +80,26 @@ class DbVirtual implements IDbAdapter
 
     public function beginTransaction()
     {
+        if (!is_null($this->tables_transaction))
+            throw new \Exception(self::ERROR_TRANSACTION);
 
+        $this->tables_transaction = $this->tables;
     }
 
     public function commit()
     {
+        if (is_null($this->tables_transaction))
+            throw new \Exception(self::ERROR_NO_TRANSACTION);
 
+        $this->tables_transaction = null;
     }
 
     public function rollBack()
     {
+        if (is_null($this->tables_transaction))
+            throw new \Exception(self::ERROR_NO_TRANSACTION);
 
+        $this->tables = $this->tables_transaction;
     }
 
     public function query(\DbSelect $select)
--- tests/DbVirtualTest.php
+++ tests/DbVirtualTest.php
@@ -44,4 +44,60 @@ class Test_DbSql extends Test_Abstract
             }
         }
     }
+
+    public function testUpdate()
+    {
+        $db = new \DbVirtual();
+
+        for ($i = 0; $i < 10; $i++)
+        {
+            // generate table
+            $table = 'tbl' . rand($i * 100, $i * 100 + 99);
+
+            // generate keys
+            $keys = array();
+            $mkey = 'k' . rand(100, 499);
+            $mkey2 = 'k' . rand(500, 999);
+            $mvalue = rand(1000, 9999);
+            for ($j = rand(2, 5); $j >= 0; $j--)
+                $keys[] = 'k' . rand($j * 10, $j * 10 + 9);
+
+            $tcontent = array();
+            for ($j = 0; $j < 10; $j++)
+            {
+                $data = array($mkey => $mvalue, $mkey2 => $this->randValue());
+                foreach ($keys as $key)
+                    $data[$key] = $this->randValue();
+
+                // insert and check
+                $db->insert($table, $data);
+                $id = $data['id'] = $db->lastInsertId();
+                $tcontent[$id] = $data;
+                $this->assertEquals($tcontent, $db->tables[$table]);
+
+                // update single record by ID
+                foreach ($keys as $key)
+                    $data[$key] = $this->randValue();
+                $tcontent[$id] = $data;
+                $db->update($table, $data, array('id' => $id));
+                $this->assertEquals($tcontent, $db->tables[$table]);
+
+                // update all records by mkey
+                $mvalue2 = $this->randValue();
+                foreach ($tcontent as $id2 => $data)
+                    $tcontent[$id2][$mkey2] = $mvalue2;
+                $db->update($table, array($mkey2 => $mvalue2), array($mkey => $mvalue));
+                $this->assertEquals($tcontent, $db->tables[$table]);
+
+                // empty update by non-matching keys
+                $db->update($table, array($mkey2 => 0), array($mkey => $mvalue . '.bad', 'id' => $id));
+                $this->assertEquals($tcontent, $db->tables[$table]);
+
+                // single update by two keys
+                $tcontent[$id][$mkey2] = 0;
+                $db->update($table, array($mkey2 => 0), array($mkey => $mvalue, 'id' => $id));
+                $this->assertEquals($tcontent, $db->tables[$table]);
+            }
+        }
+    }
 }
